5cec6ef2a06fe913681d4b076c23bdebcd99ce0c
lrhm <ilaimihar@gmail.com>
2022-11-28 16:25:47 +0100

    updates

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,42 +1,77 @@
+import { isDisabled } from "@testing-library/user-event/dist/utils";
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
 
 function Home() {
+  const openInNewTab = url => {
+    console.log("test");
+    window.open(url, '_blank', 'noopener,noreferrer');
+  };
   return (
     <section>
       <Container fluid className="home-section" id="home">
         <Particle />
         <Container className="home-content">
           <Row>
-            <Col md={7} className="home-header">
+            <Col md={5} className="home-d-flex">
+
               <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi There!{" "}
-                <span className="wave" role="img" aria-labelledby="wave">
-                  👋🏻
-                </span>
+                Please help{" "}
+             
               </h1>
 
-              <h1 className="heading-name">
-                I'M
-                <strong className="main-name"> SOUMYAJIT BEHERA</strong>
-              </h1>
+              {/* <h1 className="heading-name">
+                
+              <strong className="main-name"> Woman Life Liberty</strong>
+              </h1> */}
 
-              <div style={{ padding: 50, textAlign: "left" }}>
+              <div style={{ padding: 50, paddingTop:10, textAlign: "left" }}>
                 <Type />
               </div>
+
+              <div style = {{paddingTop: 20}}>
+                {/* a button div that says iranian scholars for liberty petition */}
+
+                <div className="heading-name">By signing pettions:</div>
+            
+                
+                
+                </div>
+                <div  style={{paddingTop:30}} className="heading-name" >And sharing our voice!</div>
+
             </Col>
 
-            <Col md={5} style={{ paddingBottom: 20 }}>
+
+            <Col md={3} style={{ paddingBottom: 20, paddingLeft:50 }} className="home-d-flex" onClick={ (e) => openInNewTab("test.com") } >
+             
+          
+                {/* opens a web page on lick */}
+             <div style={{paddingTop: 60, fontSize: 40}} aria-hidden="true" onClick={ () =>  openInNewTab("test.com")}
+               >
+              Sign Iranian Scholars for Liberty Statement
+            </div>
+
+            
+       
+       
+            </Col>
+            <Col md={3} style={{ paddingBottom: 20, paddingLeft:50 }}  className="home-d-flex" >
+            <div style={{paddingTop: 60, fontSize: 40}}>
+             Free Iranian protest rapper Toomaj Salehi
+            </div>
+              {/* <div style={{ textAlign: "center" }}>
+                test
+              </div>
               <img
                 src={homeLogo}
                 alt="home pic"
                 className="img-fluid"
                 style={{ maxHeight: "450px" }}
-              />
+              /> */}
             </Col>
           </Row>
         </Container>
--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,19 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Developer",
-          "Deep Learning Engineer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Femme, Vie, Liberté",
+          "Woman Life Liberty",
+          "زن زندگی آزادی",
+          "Woman Life Freedom",
+          "Jin, Jiyan, Azadî",
+          "Vrouw Leven Vrijheid",
+          "امرأة، حياة، حرية",
+          "Frau, Leben, Freiheit",
+          "Mujer, Vida, Libertad",
+          "Kadın, Yaşam, Özgürlük",
+          "女性、生命、自由"
+
+
         ],
         autoStart: true,
         loop: true,
